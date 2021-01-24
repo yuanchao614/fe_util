6f89cba584c23ab30ec91e88d94c950057521a2b
yuanchao614 <Micheleyuan@163.com>
2021-01-24 15:36:49 +0800

    add common.ts date.ts email.ts

---
+++ src/date.ts
@@ -0,0 +1,28 @@
+/**
+ * 返回前day天的日期2020-09-16 (if current date is 2020-10-06)
+ * @param day 
+ */
+export function daysAgo(day: number): string {
+  let d = new Date();
+  d.setDate(d.getDate() - Math.abs(day));
+  return d.toISOString().split('T')[0];
+}
+
+/**
+ * 返回从今天起后day天的日期
+ * @param day 
+ */
+export function daysFromNow(day: number): string {
+  let d = new Date();
+  d.setDate(d.getDate() + Math.abs(day));
+  return d.toISOString().split('T')[0];
+}
+
+/**
+ * 返回今天是星期几
+ * @param date 
+ * @param locale 
+ */
+export function dayName(date: any, locale = undefined) {
+  date.toLocaleDateString(locale, { weekday: 'long' });
+}
--- src/public-api.ts
+++ src/public-api.ts
@@ -1,1 +1,7 @@
 export * from './array'
+export * from './common'
+export * from './date'
+export * from './email'
+export * from './number'
+
+
