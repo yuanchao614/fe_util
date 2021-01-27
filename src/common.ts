8745ec8950f1facb96ced9f0914111326a77e444
yuan.chao <2428306489@qq.com>
2021-01-27 18:06:43 +0800

    add new common methods

--- src/common.ts
+++ src/common.ts
@@ -4,6 +4,13 @@
 export const isBrowser = typeof window !== 'undefined';
 
 /**
+ * Object.prototype.toString.call(value) 用于精确判断value的类型
+ */
+export const objectToString = Object.prototype.toString;
+
+export const toTypeString = (value: unknown): string => objectToString.call(value)
+
+/**
  * 判断是否是function
  * @param val 
  */
@@ -12,21 +19,75 @@ export function isFunction(val: unknown): val is Function {
 }
 
 /**
- * 
- * @param val 判断是否是object
+ * 判断是否是object
+ * @param val
  */
 export function isObject(val: unknown): val is Record<any, any> {
   return val !== null && typeof val === 'object';
 }
 
 /**
- * 
- * @param val 判断是否是Promise
+ * 判断是否是Promise
+ * @param val
  */
 export function isPromise<T = any>(val: unknown): val is Promise<T> {
   return isObject(val) && isFunction(val.then) && isFunction(val.catch);
 }
 
-export const trim = function(s: string) {
+/**
+ * 判断是否是Date类型
+ * @param val
+ */
+
+export function isDate(val: unknown): val is Date {
+  return val instanceof Date
+}
+
+/**
+ * 判断是否是string
+ * @param val 
+ */
+export function isString(val: unknown): val is string {
+  return typeof val === 'string';
+}
+
+/**
+ * 判断是否是symbol
+ * @param val 
+ */
+export function isSymbol(val: unknown): val is symbol {
+  return typeof val === 'symbol';
+}
+
+/**
+ * 判断是否是number
+ * @param val
+ */
+
+export function isNumber(val: unknown): val is number {
+  return typeof val === 'number';
+}
+
+/**
+ * 判断是否是map
+ * @param val
+ */
+
+export function isMap(val: unknown): val is Map<any, any> {
+  return toTypeString(val) === '[object Map]';
+}
+
+/**
+ * 判断是否是set
+ * @param val 
+ */
+export function isSet(val: unknown): val is Set<any> {
+  return toTypeString(val) === '[object Set]';
+}
+
+/** 
+ * 去除字符串前后空格
+*/
+export const trim = function (s: string) {
   return (s || '').replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g, '')
 }
